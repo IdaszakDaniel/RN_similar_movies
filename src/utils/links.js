a5ef29af59719b7f3e33d6b0ca6e47673fcfd6ca
IdaszakDaniel <idaszak1@gmail.com>
2020-04-16 12:51:13 +0200

    linter fixes

--- src/utils/links.js
+++ src/utils/links.js
@@ -1,5 +1,3 @@
-export const tastedive = (key, type, result) =>
-  `https://tastedive.com/api/similar?k=${key}&type=${type}&q=${result}`
+export const tastedive = (key, type, result) => `https://tastedive.com/api/similar?k=${key}&type=${type}&q=${result}`
 
-export const themoviedb = (query, key) =>
-  `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${key}`
+export const themoviedb = (query, key) => `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${key}`
