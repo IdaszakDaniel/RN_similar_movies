eaaed12e199f98b0c34ecf4a977375d43b2c62bb
IdaszakDaniel <idaszak1@gmail.com>
2020-04-19 14:00:30 +0200

    moviesList typings added; typescript setup

--- src/components/moviesListComponent/moviesList.tsx
+++ src/components/moviesListComponent/moviesList.tsx
@@ -1,18 +1,42 @@
 import React from 'react'
 import { RkChoice } from 'react-native-ui-kitten'
-import {
-  Text,
-  FlatList,
-  View
-} from 'react-native'
+import { Text, FlatList, View } from 'react-native'
 import styles from './moviesListComponent.styles'
+import { curry } from 'lodash'
 
-const MoviesList = ({ moviesList, selectItem }) => {
-  const keyExtractor = item => item.data.id.toString()
+type itemType = {
+  item: singleItemType
+}
+
+type singleItemType = {
+  title: string
+  data: {
+    id: number
+  }
+}
+
+type keyType = {
+  data: {
+    id: number
+  }
+  title: string
+}
+
+export interface Props {
+  moviesList: ReadonlyArray<keyType>
+  selectItem: Function
+}
+
+const MoviesList: React.FC<Props> = ({ moviesList, selectItem }) => {
+  const keyExtractor = (item: keyType): string => item.data.id.toString()
+
+  const setOnChange = curry((item: singleItemType, val: Boolean): void =>
+    selectItem(item, val),
+  )
 
   const { itemContainer, checkBox, itemTitle, listWrapper } = styles
 
-  const renderItem = ({ item }) => (
+  const renderItem = ({ item }: itemType): React.ReactElement => (
     <View style={itemContainer}>
       <View style={itemTitle}>
         <Text>{item.title}</Text>
@@ -20,7 +44,7 @@ const MoviesList = ({ moviesList, selectItem }) => {
       <RkChoice
         rkType="clear"
         selected={false}
-        onChange={val => selectItem(val, item)}
+        onChange={setOnChange(item)}
         style={checkBox}
       />
     </View>
